be9d72d727fcedaaf5e7bfef2c537379aa5e72d3
Gleb Averchuk <altermn@gmail.com>
2014-03-25 21:52:02 +0400

    extract missing products to method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,11 @@ class WelcomeController < ApplicationController
 
   before_filter :find_products, only: :index
 
+  def initialize
+    super
+    @products_limit ||= Settings.counts.welcome_products.to_i
+  end
+
   def index
     @page = Page.welcome
   end
@@ -16,23 +21,23 @@ class WelcomeController < ApplicationController
       else
         Product.scoped
       end
-    products_limit = Settings.counts.welcome_products.to_i
+
     @products = []
     @products += not_ordered_scope.novelties.limit_random(1)
-    @products += not_ordered_scope.sales.limit_random(1) if products_limit > 1
+    @products += not_ordered_scope.sales.limit_random(1) if @products_limit > 1
 
-    missing_products = -> scope do
-      scope.not_products(@products).limit_random(products_limit - @products.size)
+    if @products.size < @products_limit
+      @products += missing_products(not_ordered_scope.not_archived)
     end
 
-    if @products.size < products_limit
-      @products += missing_products[not_ordered_scope.not_archived]
-    end
-
-    if @products.size < products_limit
-      @products += missing_products[Product.scoped]
+    if @products.size < @products_limit
+      @products += missing_products(Product.scoped)
     end
 
     @products.shuffle!
+  end
+
+  def missing_products(scope)
+    scope.not_products(@products).limit_random(@products_limit - @products.size)
   end
 end
